669887acdf36bd07d36d2ee3621c856c35116a88
Daniel <danielrudn@gmail.com>
2016-12-22 22:39:12 -0500

    fixed quote padding on mobile

--- src/js/components/Quote.jsx
+++ src/js/components/Quote.jsx
@@ -15,7 +15,7 @@ export default class Quote extends Component {
               <p>Fill out the form with your details and<br/>we'll reach out to you as soon as possible.</p>
             </div>
           </div>
-          <div className="col-md-6 col-sm-6 col-xs-12" style={{paddingTop: '5%'}}>
+          <div className="col-md-6 col-sm-6 col-xs-12" style={{paddingTop: '10%', paddingBottom: '15%'}}>
             <QuoteForm/>
           </div>
         </div>
